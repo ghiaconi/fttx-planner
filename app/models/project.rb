895947392613cd011a44421dc0ef0fda916f752a
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2021-05-13 10:25:30 +0200

    projects and associations update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ApplicationRecord
 
-  has_many :teams, through: tasks
   has_many :materials
+  has_many :tasks
+
+  has_many :users, through: :tasks
 
   has_many_attached :files
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: ["open", "ongoing", "closed", "blocked"] }
 
   belongs_to :project
-  belongs_to :team
+  has_many :users
 
   has_many_attached :files
 end
