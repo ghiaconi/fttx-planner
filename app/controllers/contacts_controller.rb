14b8890ffdabb8d85f466ddc3b83a5fe0b0fda12
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2021-06-10 19:39:18 +0200

    landing text update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
       @contact.deliver
       @contact = Contact.new
       redirect_to root_path
-      flash[:notice] = "Am primit mesajul tau si iti vom raspunde cat mai curand"
+      flash[:notice] = "We got your message and we'll be back to you asap!"
     else
       render :new
     end
